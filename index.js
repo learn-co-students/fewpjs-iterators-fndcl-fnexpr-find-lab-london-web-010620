afcd16faee2c2eec0a7d5e6c5f86a604b04fd4f3
Wojciech Bojarski <bojawojt@gmail.com>
2020-02-23 16:34:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
-}
+const superbowlWin = record => {
+  let found = record.find(element => {
+    return element.result === "W";
+  });
+  if (found) return found.year;
+};
